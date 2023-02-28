eaf018658f9855181c5729d9d4c626adb519e987
Jayvee Milana <jmmilana12@gmail.com>
2023-02-28 10:39:07 -0600

    add js

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,9 +3,13 @@
 /*
   Guessing Game
 */
+const form = document.querySelector('form');
+const output = document.querySelector('.output p');
+const guess = document.querySelector('.guess');
+
 
 function isNumber(num) {
-  if (num.length > 0 && !isNaN(num)) {
+  if (num.length > 0 && !isNaN(num) && num <= 50 & num >= 0) {
     return true;
   }
   return false;
@@ -15,4 +19,16 @@ function randomNumber() {
   return Math.floor(Math.random() * 50);
 }
 
-randomNumber();
+guess.addEventListener('click', function() {
+  if (isNumber(a)) {
+    if (isNumber(a) < randomNumber()) {
+      output.innerText = 'My number is bigger';
+    } else if (isNumber(a) > randomNumber()) {
+      output.innerText = 'My number is smaller';
+    } else {
+      output.innerText = 'That\'s my number!'
+    }
+  } else {
+    output.innerText = 'Please, enter valid a valid number between 1 and 50.';
+  }
+});

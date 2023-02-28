767072bacccce6d85b785449c5b3a479c2091091
Jayvee Milana <jmmilana12@gmail.com>
2023-02-28 11:30:27 -0600

    js update

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,7 +8,6 @@ const output = document.querySelector('.output p');
 const guess = document.querySelector('.guess');
 const number = document.querySelector('.number');
 
-
 function isNumber(num) {
   if (num.length > 0 && !isNaN(num) && num <= 50 & num >= 0) {
     return true;
@@ -22,14 +21,18 @@ function randomNumber() {
 
 guess.addEventListener('click', function() {
   let a = number.value.trim();
+  let b = randomNumber();
 
   if (isNumber(a)) {
-    if (isNumber(a) < randomNumber()) {
+    if (isNumber(a) < b) {
       output.innerText = 'My number is bigger';
-    } else if (isNumber(a) > randomNumber()) {
+      number.value = '';
+    } else if (isNumber(a) > b) {
       output.innerText = 'My number is smaller';
+      number.value = '';
     } else {
-      output.innerText = 'That\'s my number!'
+      output.innerText = 'That\'s my number!';
+      number.value = '';
     }
   } else {
     output.innerText = 'Please, enter valid a valid number between 1 and 50.';

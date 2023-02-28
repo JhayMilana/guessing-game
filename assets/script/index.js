c61ba0f136447b159d95d0ba5422166c94e94d23
Jayvee Milana <jmmilana12@gmail.com>
2023-02-28 12:43:11 -0600

    js update

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,9 +4,9 @@
   Guessing Game
 */
 const form = document.querySelector('form');
-const output = document.querySelector('.output p');
 const guess = document.querySelector('.guess');
 const number = document.querySelector('.number');
+const output = document.querySelector('.output p');
 
 function isNumber(num) {
   if (num.length > 0 && !isNaN(num) && num <= 50 & num >= 0) {
@@ -22,15 +22,16 @@ function randomNumber() {
 guess.addEventListener('click', function() {
   let a = number.value.trim();
   let b = randomNumber();
+  let c = isNumber(a);
 
   if (isNumber(a)) {
-    if (isNumber(a) < b) {
+    if (c < b) {
       output.innerText = 'My number is bigger';
       number.value = '';
-    } else if (isNumber(a) > b) {
+    } else if (c > b) {
       output.innerText = 'My number is smaller';
       number.value = '';
-    } else {
+    } else if (c = b) {
       output.innerText = 'That\'s my number!';
       number.value = '';
     }
